8e898e012d140f02b8113bc71367191706157ac3
Chu Thanh Dat <dat.chuthanh@mobifone.vn>
2023-07-12 09:25:11 +0700

    123

--- src/excalidraw-app/data/firebase.ts
+++ src/excalidraw-app/data/firebase.ts
@@ -8,81 +8,6 @@ import { FILE_CACHE_MAX_AGE_SEC } from "../app_constants";
 import { decompressData } from "../../data/encode";
 import { MIME_TYPES } from "../../constants";
 
-// private
-// -----------------------------------------------------------------------------
-
-let FIREBASE_CONFIG: Record<string, any>;
-try {
-  FIREBASE_CONFIG = JSON.parse(process.env.REACT_APP_FIREBASE_CONFIG);
-} catch (error: any) {
-  console.warn(
-    `Error JSON parsing firebase config. Supplied value: ${process.env.REACT_APP_FIREBASE_CONFIG}`,
-  );
-  FIREBASE_CONFIG = {};
-}
-
-let firebasePromise: Promise<typeof import("firebase/app").default> | null =
-  null;
-let firebaseStoragePromise: Promise<any> | null | true = null;
-
-let isFirebaseInitialized = false;
-
-const _loadFirebase = async () => {
-
-  const firebase = (
-    await import(/* webpackChunkName: "firebase" */ "firebase/app")
-  ).default;
-
-  if (!isFirebaseInitialized) {
-    try {
-      firebase.initializeApp(FIREBASE_CONFIG);
-    } catch (error: any) {
-      // trying initialize again throws. Usually this is harmless, and happens
-      // mainly in dev (HMR)
-      if (error.code === "app/duplicate-app") {
-        console.warn(error.name, error.code);
-      } else {
-        throw error;
-      }
-    }
-    isFirebaseInitialized = true;
-  }
-  // console.log("_loadFirebase", firebase)
-  return firebase;
-};
-
-const _getFirebase = async (): Promise<
-  typeof import("firebase/app").default
-> => {
-
-  if (!firebasePromise) {
-    firebasePromise = _loadFirebase();
-  }
-  // console.log("_getFirebase", firebasePromise)
-
-  return firebasePromise;
-};
-
-// -----------------------------------------------------------------------------
-
-
-export const loadFirebaseStorage = async () => {
-
-  const firebase = await _getFirebase();
-  if (!firebaseStoragePromise) {
-    firebaseStoragePromise = import(
-      /* webpackChunkName: "storage" */ "firebase/storage"
-    );
-  }
-  if (firebaseStoragePromise !== true) {
-    await firebaseStoragePromise;
-    firebaseStoragePromise = true;
-  }
-
-  return firebase;
-};
-
-
 export const saveFilesToFirebase = async ({
   prefix,
   files,
@@ -90,25 +15,11 @@ export const saveFilesToFirebase = async ({
   prefix: string;
   files: { id: FileId; buffer: Uint8Array }[];
 }) => {
-  const firebase = await loadFirebaseStorage();
   const erroredFiles = new Map<FileId, true>();
   const savedFiles = new Map<FileId, true>();
   await Promise.all(
     files.map(async ({ id, buffer }) => {
       try {
-        await firebase
-          .storage()
-          .ref(`${prefix}/${id}`)
-          .put(
-            new Blob([buffer], {
-              type: MIME_TYPES.binary,
-            }),
-            {
-              cacheControl: `public, max-age=${FILE_CACHE_MAX_AGE_SEC}`,
-            },
-          );
-        savedFiles.set(id, true);
-
         //Custome Load SV
         const formData = new FormData();
         const blobFile = new Blob([buffer], {
@@ -119,6 +30,8 @@ export const saveFilesToFirebase = async ({
           method: "Post",
           body: formData,
         });
+
+        savedFiles.set(id, true);
         //
       } catch (error: any) {
         erroredFiles.set(id, true);
@@ -139,20 +52,17 @@ export const loadFilesFromFirebase = async (
   await Promise.all(
     [...new Set(filesIds)].map(async (id) => {
       try {
-        const url = `https://firebasestorage.googleapis.com/v0/b/${FIREBASE_CONFIG.storageBucket
-          }/o/${encodeURIComponent(prefix.replace(/^\//, ""))}%2F${id}`;
-        const response = await fetch(`${url}?alt=media`);
-        if (response.status < 400) {
-          console.log("response 1 ", response)
-          const arrayBuffer = await response.arrayBuffer();
-
+        const response2 = await fetch(`http://localhost:3101/api/v1/images/getfile?idField=${id}`, {
+          method: "Get",
+        });
+        if (response2.status < 400) {
+          const arrayBuffer = await response2.arrayBuffer();
           const { data, metadata } = await decompressData<BinaryFileMetadata>(
             new Uint8Array(arrayBuffer),
             {
               decryptionKey,
             },
           );
-
           const dataURL = new TextDecoder().decode(data) as DataURL;
 
           loadedFiles.push({
@@ -162,34 +72,6 @@ export const loadFilesFromFirebase = async (
             created: metadata?.created || Date.now(),
             lastRetrieved: metadata?.created || Date.now(),
           });
-
-          //Custome Code....
-          const response2 = await fetch(`http://localhost:3101/api/v1/images/getfile?idField=${id}`, {
-            method: "Get",
-          });
-          console.log("response 2 ", response2)
-          // const arrayBuffer = await response2.arrayBuffer();
-
-          // const { data, metadata } = await decompressData<BinaryFileMetadata>(
-          //   new Uint8Array(arrayBuffer),
-          //   {
-          //     decryptionKey,
-          //   },
-          // );
-
-          // const dataURL = new TextDecoder().decode(data) as DataURL;
-
-          // loadedFiles.push({
-          //   mimeType: metadata.mimeType || MIME_TYPES.binary,
-          //   id,
-          //   dataURL,
-          //   created: metadata?.created || Date.now(),
-          //   lastRetrieved: metadata?.created || Date.now(),
-          // });
-
-
-
-
         } else {
           erroredFiles.set(id, true);
         }
